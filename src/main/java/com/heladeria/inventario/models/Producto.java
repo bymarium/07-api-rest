30bd96f9179ecbbfba46dbe96d4daf35f180dae3
shermsofka <116570770+shermsofka@users.noreply.github.com>
2025-01-03 15:07:46 -0500

    Create Producto.java

---
+++ src/main/java/com/heladeria/inventario/models/Producto.java
@@ -0,0 +1,36 @@
+package com.heladeria.inventario.models;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String categoria;
+    private Integer cantidad;
+
+    public Producto(Long id, String nombre, String categoria, Integer cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.cantidad = cantidad;
+    }
+
+    public Producto() {
+    }
+
+
+}
