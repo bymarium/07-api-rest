0dd9e2c995a9aaa70b3867ddf7f24c234f1c65e3
shermsofka <116570770+shermsofka@users.noreply.github.com>
2025-01-03 14:59:24 -0500

    Create ProductoRequestDTO.java

---
+++ src/main/java/com/example/demo/dto/ProductoRequestDTO.java
@@ -0,0 +1,32 @@
+package com.example.demo.dto;
+
+
+public class ProductoRequestDTO {
+    private String nombre;
+    private String categoria;
+    private Integer cantidad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+}
