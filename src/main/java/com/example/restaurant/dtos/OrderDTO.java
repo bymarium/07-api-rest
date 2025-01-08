6ccd7b2898aa1f18ff7cfbcb9e571d280e58be88
Bymarium <bymarium@gmail.com>
2025-01-07 23:43:29 -0500

    feat(dtos): add empty constructor to dtos

--- src/main/java/com/example/restaurant/dtos/OrderDTO.java
+++ src/main/java/com/example/restaurant/dtos/OrderDTO.java
@@ -1,12 +1,14 @@
 package com.example.restaurant.dtos;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class OrderDTO {
 	private Long clientId;
 	private List<OrderDetailDTO> orderDetails;
