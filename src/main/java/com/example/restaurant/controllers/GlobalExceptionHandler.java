9b20fb682a30e060161182326a1bacfa3114f4da
Bymarium <bymarium@gmail.com>
2025-01-09 14:51:55 -0500

    feat(config): create documentation in swagger

--- src/main/java/com/example/restaurant/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/example/restaurant/controllers/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.example.restaurant.controllers;
 
 import com.example.restaurant.dtos.MessageDTO;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
